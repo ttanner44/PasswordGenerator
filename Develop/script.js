3151fd56441effe0e9c26c66f072e4d13ec3d305
Tim Tanner <timtanner@tanner-companies.com>
2020-04-18 09:13:48 -0500

    added base password functionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,14 @@
-// Assignment Code
+// Assignment Code - DOM Elements
+var passwordText = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
-var passwordOutput = "";
 
 // Write password to the #password input
 function writePassword() {
     
   //Need to add validation and loop to confirm numeric and in proper range
   var pwlength = parseInt(prompt("How large to you want your password? (8-128 characters)?"));
-    console.log("pwlength = " + pwlength); 
+    console.log("pwlength = " + pwlength);
+    console.log(typeof pwlength); 
 
   // Confirm requires no validation as it's boolean
   var pwupper = confirm("Do you want upper case characters?...OK for yes?");
@@ -19,59 +20,59 @@ function writePassword() {
   var pwsymbol = confirm("Do you want symbols?...OK for yes?");
     console.log("pwsymbol = " + pwsymbol);
 
-  passwordOutput.innerText = generatePassword(pwupper, pwlower, pwnumber, pwsymbol, pwlength);
-    
-  var password = generatePassword();
+  var password = generatePassword(pwupper, pwlower, pwnumber, pwsymbol, pwlength);
   
-  var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-}
 
+}  
+  
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
 // Generate Password function
-function generatePassword(upper, lower, number, symbol, length) {
+function generatePassword(pwupper, pwlower, pwnumber, pwsymbol, pwlength) {
     //1. Init PW var
-    //2. Filter out unselected variable types types.
+
+    //2. Filter out unselected variable types.
+    
     //3. Loop over length call generator function for each type.
+    
     //4. Add final pw to password variable and return
     
-    var generatedPassword = ' ';
+    var generatedPassword = '';
+    var typesCount = pwupper + pwlower + pwnumber + pwsymbol;
+    console.log("typesCount", typesCount);
 
-    var typesCount = upper + lower + number + symbol;
-    
-    console.log('typeCount: ', typesCount);
-
-    const typesArr = [{ upper }, { lower }, { number }, { symbol }].filter
-      (
-        item => Object.values(item) [0]
-      );
-
-    console.log('typesArr: ', typesArr);
+    var typesArr = [{pwupper}, {pwlower}, {pwnumber}, {pwsymbol}].filter 
+    (item => Object.values(item)[0]);
+    console.log("typesArr: ", typesArr);
 
     if(typesCount === 0 ) {
         return "";
     }
 
-    for (let i=0; i < length; i+= typesCount) {
+    for (var i=0; i < pwlength; i += typesCount) {
         typesArr.forEach(type => {
           var funcName = Object.keys(type)[0];
-          console.log('funcname: ', funcName);
-          generatePassword += randomFunc[funcName]();
+
+          console.log('funcName: ', funcName);
+          
+          generatedPassword += randomFunc[funcName]();
         });
     }
+    console.log(generatedPassword.slice(0,pwlength));
+    var finalPassword = generatedPassword.slice(0,pwlength);
+    return finalPassword;
 }
 
 
 // Gernerator Functions
 var randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
+  pwlower: getRandomLower,
+  pwupper: getRandomUpper,
+  pwnumber: getRandomNumber,
+  pwsymbol: getRandomSymbol
 };
 
 function getRandomLower () {
